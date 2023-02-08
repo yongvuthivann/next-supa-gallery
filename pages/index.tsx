7beaa29c2a58d22c17af0bc0cbadca5e71395653
Vuthivann Yong <vuthivann.yong007@gmail.com>
2023-02-08 17:10:46 +0700

    ignore commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,7 @@ const Home = ({ images }: props) => {
         </h2>
         <div className="cursor-pointer">
           <Link href={"https://github.com/yongvuthivann/next-supa-gallery"}>
-            <AiOutlineGithub className="w-8 h-8" />
+            <AiOutlineGithub className="w-8 h-8 hover:opacity-70" />
           </Link>
         </div>
       </div>
