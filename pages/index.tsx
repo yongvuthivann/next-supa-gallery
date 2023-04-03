3ad8712180514842549369a322576d7c847f8014
Vuthivann Yong <vuthivann.yong007@gmail.com>
2023-04-03 11:29:25 +0700

    [

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ const Home = ({ images }: props) => {
       </div>
 
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {images.map((image) => (
+        {images?.map((image) => (
           <BlurImage image={image} key={image.id} />
         ))}
       </div>
